cdc40e12b4b4ce1560db22c9986519e24d6f4f21
Jivvy <dd300268@gmail.com>
2023-05-25 00:07:08 -0500

    Fix crash on launch due to using deprecated addLayer for sand filter block

--- src/main/java/dev/ghen/thirst/compat/create/CreateRegistry.java
+++ src/main/java/dev/ghen/thirst/compat/create/CreateRegistry.java
@@ -1,19 +1,13 @@
 package dev.ghen.thirst.compat.create;
 
-import com.simibubi.create.AllBlocks;
-import com.simibubi.create.AllTileEntities;
 import com.simibubi.create.Create;
 import com.simibubi.create.content.contraptions.components.AssemblyOperatorBlockItem;
-import com.simibubi.create.content.contraptions.fluids.actors.SpoutBlock;
-import com.simibubi.create.content.contraptions.fluids.actors.SpoutTileEntity;
 import com.simibubi.create.foundation.data.AssetLookup;
 import com.simibubi.create.foundation.data.SharedProperties;
 import com.tterrag.registrate.util.entry.BlockEntityEntry;
 import com.tterrag.registrate.util.entry.BlockEntry;
-import dev.ghen.thirst.content.registry.ItemInit;
 import net.minecraft.client.renderer.RenderType;
 
-import static com.simibubi.create.AllTags.pickaxeOnly;
 import static com.simibubi.create.foundation.data.ModelGen.customItemModel;
 
 public class CreateRegistry
@@ -28,7 +22,6 @@ public class CreateRegistry
         SAND_FILTER_BLOCK = Create.REGISTRATE.block("sand_filter", SandFilterBlock::new)
                 .initialProperties(SharedProperties::copperMetal)
                 .blockstate((ctx, prov) -> prov.simpleBlock(ctx.getEntry(), AssetLookup.partialBaseModel(ctx, prov)))
-                .addLayer(() -> RenderType::cutoutMipped)
                 .item(AssemblyOperatorBlockItem::new)
                 .transform(customItemModel())
                 .register();
