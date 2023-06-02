176a0bff127ecf4cd8569accba8b24252d39f270
mlus <1319237806@qq.com>
2023-06-03 01:16:47 +0800

    Create CreateRegistry.java

---
+++ src/main/java/dev/ghen/thirst/compat/create/CreateRegistry.java
@@ -0,0 +1,37 @@
+package dev.ghen.thirst.compat.create;
+
+import com.simibubi.create.Create;
+import com.simibubi.create.content.processing.AssemblyOperatorBlockItem;
+import com.simibubi.create.foundation.data.AssetLookup;
+import com.simibubi.create.foundation.data.SharedProperties;
+import com.tterrag.registrate.builders.BlockEntityBuilder;
+import com.tterrag.registrate.util.entry.BlockEntityEntry;
+import com.tterrag.registrate.util.entry.BlockEntry;
+import net.minecraft.client.renderer.RenderType;
+
+import static com.simibubi.create.foundation.data.ModelGen.customItemModel;
+
+public class CreateRegistry
+{
+    public CreateRegistry(){}
+    public static void register(){}
+    public static final BlockEntry<SandFilterBlock> SAND_FILTER_BLOCK;
+    public static final BlockEntityEntry<SandFilterTileEntity> SAND_FILTER_TE;
+
+
+    static
+    {
+        SAND_FILTER_BLOCK = Create.REGISTRATE.block("sand_filter", SandFilterBlock::new)
+                .initialProperties(SharedProperties::copperMetal)
+                .blockstate((ctx, prov) -> prov.simpleBlock(ctx.getEntry(), AssetLookup.partialBaseModel(ctx, prov)))
+                //.addLayer(()->RenderType::cutoutMipped)
+                .item(AssemblyOperatorBlockItem::new)
+                .transform(customItemModel())
+                .register();
+
+        SAND_FILTER_TE = Create.REGISTRATE
+                .blockEntity("sand_filter",BlockEntityBuilder.BlockEntityFactory(SandFilterTileEntity))
+                .validBlocks(SAND_FILTER_BLOCK)
+                .register();
+    }
+}
