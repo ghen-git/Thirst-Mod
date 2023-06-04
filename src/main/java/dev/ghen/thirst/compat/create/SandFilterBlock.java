fe8ebfa63bcc847c2e81c757ca75d8af5de7c64f
mlus-Asuka <1319237806@qq.com>
2023-06-04 14:04:42 +0800

    fix render bug

--- src/main/java/dev/ghen/thirst/compat/create/SandFilterBlock.java
+++ src/main/java/dev/ghen/thirst/compat/create/SandFilterBlock.java
@@ -1,21 +1,34 @@
 package dev.ghen.thirst.compat.create;
 
+import com.simibubi.create.AllShapes;
 import com.simibubi.create.content.equipment.wrench.IWrenchable;
 import com.simibubi.create.foundation.advancement.AdvancementBehaviour;
 import com.simibubi.create.foundation.block.IBE;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
+@SuppressWarnings("deprecation")
 public class SandFilterBlock extends Block implements IWrenchable, IBE<SandFilterTileEntity> {
 
     public SandFilterBlock(Properties p_i48440_1_) {
         super(p_i48440_1_);
+    }
+
+    @Override
+    public @NotNull VoxelShape getShape(@NotNull BlockState p_220053_1_, @NotNull BlockGetter p_220053_2_, @NotNull BlockPos p_220053_3_,
+                                        @NotNull CollisionContext p_220053_4_) {
+        return AllShapes.SPOUT;
     }
 
     @Override
@@ -33,4 +46,17 @@ public class SandFilterBlock extends Block implements IWrenchable, IBE<SandFilte
     public BlockEntityType<? extends SandFilterTileEntity> getBlockEntityType() {
         return CreateRegistry.SAND_FILTER_TE.get();
     }
+
+    @Override
+    public InteractionResult onWrenched(BlockState state, UseOnContext context) {
+        return IWrenchable.super.onWrenched(state, context);
+    }
+
+    @Override
+    public InteractionResult onSneakWrenched(BlockState state, UseOnContext context) {
+        return IWrenchable.super.onSneakWrenched(state, context);
+    }
+
+
+
 }
