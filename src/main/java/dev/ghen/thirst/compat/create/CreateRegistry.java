165446bdd6c05f0ebbf6658248efa58970592597
mlus-Asuka <1319237806@qq.com>
2023-06-05 14:29:30 +0800

    wait.whether there is a situation player haven't install create so crash happens again

--- src/main/java/dev/ghen/thirst/compat/create/CreateRegistry.java
+++ src/main/java/dev/ghen/thirst/compat/create/CreateRegistry.java
@@ -3,6 +3,7 @@ package dev.ghen.thirst.compat.create;
 import com.simibubi.create.content.processing.AssemblyOperatorBlockItem;
 import com.simibubi.create.foundation.data.AssetLookup;
 import com.simibubi.create.foundation.data.SharedProperties;
+import com.simibubi.create.foundation.data.TagGen;
 import com.tterrag.registrate.util.entry.BlockEntityEntry;
 import com.tterrag.registrate.util.entry.BlockEntry;
 import dev.ghen.thirst.foundation.tab.ThirstTab;
@@ -25,6 +26,7 @@ public class CreateRegistry
     public static final BlockEntry<SandFilterBlock> SAND_FILTER_BLOCK= REGISTRATE.get()
             .block("sand_filter", SandFilterBlock::new)
             .initialProperties(SharedProperties::copperMetal)
+            .transform(TagGen.pickaxeOnly())
             .blockstate((ctx, prov) -> prov.simpleBlock(ctx.getEntry(), AssetLookup.partialBaseModel(ctx, prov)))
             //.addLayer(()->RenderType::cutoutMipped)
             .item(AssemblyOperatorBlockItem::new)
