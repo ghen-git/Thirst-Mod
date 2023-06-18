c061558f921f6d1d41c0d07075fc1df4dbb38147
mlus-Asuka <1319237806@qq.com>
2023-06-18 21:24:28 +0800

    Fix crash when drop bottle in encase fan.

--- src/main/java/dev/ghen/thirst/foundation/mixin/create/MixinFanProcessing.java
+++ src/main/java/dev/ghen/thirst/foundation/mixin/create/MixinFanProcessing.java
@@ -1,11 +1,7 @@
 package dev.ghen.thirst.foundation.mixin.create;
 
-import com.simibubi.create.AllRecipeTypes;
 import com.simibubi.create.content.kinetics.fan.FanProcessing;
-import com.simibubi.create.content.kinetics.fan.HauntingRecipe;
-import com.simibubi.create.content.kinetics.fan.SplashingRecipe;
 import dev.ghen.thirst.content.purity.WaterPurity;
-import dev.ghen.thirst.foundation.config.CommonConfig;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.AbstractCookingRecipe;
@@ -21,9 +17,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 
@@ -33,31 +29,10 @@ public class MixinFanProcessing {
     @Final
     @Shadow
     private static final RecipeWrapper RECIPE_WRAPPER = new RecipeWrapper(new ItemStackHandler(1));
-    @Final
-    @Shadow
-    private static final FanProcessing.SplashingWrapper SPLASHING_WRAPPER = new FanProcessing.SplashingWrapper();
-
-    @Final
-    @Shadow
-    private static final FanProcessing.HauntingWrapper HAUNTING_WRAPPER = new FanProcessing.HauntingWrapper();
 
 
     @Inject(method = "process",at= @At("HEAD"), cancellable = true)
     private static void process(ItemStack stack, FanProcessing.Type type, Level world, CallbackInfoReturnable<List<ItemStack>> cir) {
-        if (type == FanProcessing.Type.SPLASHING) {
-            SPLASHING_WRAPPER.setItem(0, stack);
-            Optional<SplashingRecipe> recipe = AllRecipeTypes.SPLASHING.find(SPLASHING_WRAPPER, world);
-            if (recipe.isPresent())
-                return;
-            return;
-        }
-        if (type == FanProcessing.Type.HAUNTING) {
-            HAUNTING_WRAPPER.setItem(0, stack);
-            Optional<HauntingRecipe> recipe = AllRecipeTypes.HAUNTING.find(HAUNTING_WRAPPER, world);
-            if (recipe.isPresent())
-                return;
-            return;
-        }
 
         RECIPE_WRAPPER.setItem(0, stack);
         Optional<SmokingRecipe> smokingRecipe = world.getRecipeManager()
@@ -75,29 +50,13 @@ public class MixinFanProcessing {
                     .getRecipeFor(RecipeType.SMELTING, RECIPE_WRAPPER, world);
             if (smeltingRecipe.isEmpty()) {
                 RECIPE_WRAPPER.setItem(0, stack);
-                smeltingRecipe = world.getRecipeManager()
-                        .getRecipeFor(RecipeType.BLASTING, RECIPE_WRAPPER, world);
                 if(smokingRecipe.isEmpty() && WaterPurity.hasPurity(stack)){
                     WaterPurity.addPurity(stack, Math.min(WaterPurity.getPurity(stack) + 1, WaterPurity.MAX_PURITY));
-                    cir.setReturnValue(Collections.singletonList(stack));
+                    cir.setReturnValue(new ArrayList<>(List.of(stack)));
                 }
             }
 
-            if (smeltingRecipe.isPresent()) {
-                if (smokingRecipe.isEmpty() || !ItemStack.isSame(smokingRecipe.get()
-                                .getResultItem(),
-                        smeltingRecipe.get()
-                                .getResultItem())) {
-                    return;
-                }
-            }
-
-            return;
         }
 
-        if (type == FanProcessing.Type.SMOKING && smokingRecipe.isPresent())
-            return;
-
-        return;
     }
 }
