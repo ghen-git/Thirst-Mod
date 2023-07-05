21dd18a939a04e6f8fb6a32327d52c668139fae9
Hazel Fidecaro <hazelfidecaro@gmail.com>
2023-07-05 12:02:38 +1200

    Re-add Cold Sweat compat

--- src/main/java/dev/ghen/thirst/api/ThirstHelper.java
+++ src/main/java/dev/ghen/thirst/api/ThirstHelper.java
@@ -5,46 +5,38 @@ import dev.ghen.thirst.foundation.config.CommonConfig;
 import dev.ghen.thirst.foundation.config.ItemSettingsConfig;
 import dev.ghen.thirst.foundation.util.ConfigHelper;
 import dev.ghen.thirst.foundation.util.LoadedValue;
-//import dev.momostudios.coldsweat.api.temperature.Temperature;
-//import dev.momostudios.coldsweat.api.util.TempHelper;
+import dev.momostudios.coldsweat.api.util.Temperature;
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
-import java.util.List;
 import java.util.Map;
 
 import static dev.ghen.thirst.content.purity.WaterPurity.hasPurity;
 
-public class ThirstHelper
-{
+public class ThirstHelper {
     private static final Logger LOGGER = LogUtils.getLogger();
     private static boolean useColdSweatCaps = false;
     private static final float MODIFIER_HARSHNESS = 0.5f;
     public static LoadedValue<Map<Item, Number[]>> VALID_DRINKS = LoadedValue.of(() -> ConfigHelper.getItemsWithValues(ItemSettingsConfig.DRINKS.get()));
     public static LoadedValue<Map<Item, Number[]>> VALID_FOODS = LoadedValue.of(() -> ConfigHelper.getItemsWithValues(ItemSettingsConfig.FOODS.get()));
 
-    public static boolean itemRestoresThirst(ItemStack itemStack)
-    {
-        return  isDrink(itemStack) ||
+    public static boolean itemRestoresThirst(ItemStack itemStack) {
+        return isDrink(itemStack) ||
                 isFood(itemStack);
     }
 
-    public static boolean isDrink(ItemStack itemStack)
-    {
+    public static boolean isDrink(ItemStack itemStack) {
         return !ItemSettingsConfig.ITEMS_BLACKLIST.get().contains(itemStack.getItem().toString()) &&
                 VALID_DRINKS.get().containsKey(itemStack.getItem());
     }
 
 
-    public static boolean isFood(ItemStack itemStack)
-    {
+    public static boolean isFood(ItemStack itemStack) {
         return !ItemSettingsConfig.ITEMS_BLACKLIST.get().contains(itemStack.getItem().toString()) &&
                 VALID_FOODS.get().containsKey(itemStack.getItem());
     }
@@ -52,51 +44,45 @@ public class ThirstHelper
     /**
      * Adds a hydration and "quenchness" value to an item via code, and treats it as food.
      * Can be overwritten by the player in the config.
-     * */
-    public static void addFood(Item item, int thirst, int quenched)
-    {
+     */
+    public static void addFood(Item item, int thirst, int quenched) {
         VALID_FOODS.get().put(item, new Number[]{thirst, quenched});
     }
 
     /**
      * Adds a hydration and "quenchness" value to an item via code, and treats it as a drink.
      * Can be overwritten by the player in the config.
-     * */
-    public static void addDrink(Item item, int thirst, int quenched)
-    {
+     */
+    public static void addDrink(Item item, int thirst, int quenched) {
         VALID_DRINKS.get().put(item, new Number[]{thirst, quenched});
     }
 
-    public static int getThirst(ItemStack itemStack)
-    {
+    public static int getThirst(ItemStack itemStack) {
         Item item = itemStack.getItem();
 
-        if(VALID_DRINKS.get().containsKey(item))
+        if (VALID_DRINKS.get().containsKey(item))
             return VALID_DRINKS.get().get(item)[0].intValue();
         else
             return VALID_FOODS.get().get(item)[0].intValue();
     }
 
-    public static int getQuenched(ItemStack itemStack)
-    {
+    public static int getQuenched(ItemStack itemStack) {
         Item item = itemStack.getItem();
 
-        if(VALID_DRINKS.get().containsKey(item))
+        if (VALID_DRINKS.get().containsKey(item))
             return VALID_DRINKS.get().get(item)[1].intValue();
         else
             return VALID_FOODS.get().get(item)[1].intValue();
     }
 
-    public static int getPurity(ItemStack item)
-    {
-        if(!hasPurity(item))
+    public static int getPurity(ItemStack item) {
+        if (!hasPurity(item))
             return -1;
         else
             return item.getTag().getInt("Purity");
     }
 
-    public static void shouldUseColdSweatCaps(boolean should)
-    {
+    public static void shouldUseColdSweatCaps(boolean should) {
         useColdSweatCaps = should;
     }
 
@@ -106,39 +92,35 @@ public class ThirstHelper
      * the one calculated from the mod, otherwise both parameters are entirely
      * dependent on the biome the player is standing in.
      */
-    public static float getExhaustionBiomeModifier(Player player)
-    {
+    public static float getExhaustionBiomeModifier(Player player) {
         BlockPos pos = player.getOnPos();
         Level level = player.getLevel();
 
-        if(level.dimensionType().ultraWarm())
+        if (level.dimensionType().ultraWarm())
             return 3.0f;
-        else
-        {
+        else {
             Biome biome = level.getBiome(pos).value();
 
             //humidity range: 0 - 0.8 == 0.8 midpoint: 0.4
             float humidity = biome.getDownfall() + 0.6f;
-            if(humidity <= 0.6)
+            if (humidity <= 0.6)
                 humidity += 0.5;
 
             //temperature range: -0.8 - 2 == 2.8 midpoint: 0.8
             float temp = biome.getBaseTemperature() + 0.2f;
 
-            if(useColdSweatCaps);
-                //temp = (float) (TempHelper.getTemperature(player, Temperature.Type.BODY).get() / 100f);
-            else
-            {
-                if(temp <= 0)
+            if (useColdSweatCaps) {
+                temp = (float) (Temperature.get(player, Temperature.Type.BODY) / 100f);
+            } else {
+                if (temp <= 0)
                     temp = (float) Math.exp(temp);
-                else if(temp > 1)
+                else if (temp > 1)
                     temp /= 2;
             }
 
-            float thirstModifier = CommonConfig.THIRST_DEPLETION_MODIFIER.get().floatValue() * (temp  / humidity);
+            float thirstModifier = CommonConfig.THIRST_DEPLETION_MODIFIER.get().floatValue() * (temp / humidity);
 
-            if(thirstModifier < 1)
-            {
+            if (thirstModifier < 1) {
                 float modifierOffset = 1 - thirstModifier;
                 modifierOffset *= MODIFIER_HARSHNESS;
                 thirstModifier = 1 - modifierOffset;
