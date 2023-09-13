615fad62ab82b017504b03b2be299c2891038d63
mlus-Asuka <1319237806@qq.com>
2023-09-13 16:20:41 +0800

    update to 1.3.3

--- src/main/java/dev/ghen/thirst/foundation/mixin/toughasnails/MixinServerConfig.java
+++ src/main/java/dev/ghen/thirst/foundation/mixin/toughasnails/MixinServerConfig.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import toughasnails.config.ServerConfig;
 
-@Mixin(ServerConfig.class)
+@Mixin(value = ServerConfig.class,remap = false)
 public class MixinServerConfig {
     @ModifyArg(method ="<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/common/ForgeConfigSpec$Builder;define(Ljava/lang/String;Z)Lnet/minecraftforge/common/ForgeConfigSpec$BooleanValue;", ordinal = 0),index = 1)
     private static boolean modifyBoolean(boolean defaultValue) {
