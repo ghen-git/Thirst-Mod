787aa89bbd5111c719a87c29b1684ee945536907
mlus <1319237806@qq.com>
2024-02-23 16:57:58 +0800

    Fixed #105

--- src/main/java/dev/ghen/thirst/foundation/mixin/accessors/supplementaries/FaucetBehaviorsManagerAccessor.java
+++ src/main/java/dev/ghen/thirst/foundation/mixin/accessors/supplementaries/FaucetBehaviorsManagerAccessor.java
@@ -3,7 +3,9 @@ package dev.ghen.thirst.foundation.mixin.accessors.supplementaries;
 import net.mehvahdjukaar.moonlight.api.fluids.SoftFluid;
 import net.mehvahdjukaar.moonlight.api.fluids.SoftFluidTank;
 import net.mehvahdjukaar.supplementaries.common.block.faucet.FaucetBehaviorsManager;
+import net.minecraft.core.Holder;
 import net.minecraft.nbt.CompoundTag;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
@@ -11,7 +13,7 @@ import org.spongepowered.asm.mixin.gen.Invoker;
 public interface FaucetBehaviorsManagerAccessor
 {
     @Invoker(remap = false)
-    static void invokePrepareToTransferBottle(SoftFluidTank tempFluidHolder, SoftFluid softFluid, CompoundTag tag){
+    static void invokePrepareToTransferBottle(SoftFluidTank tempFluidHolder, Holder<SoftFluid> softFluid, @Nullable CompoundTag tag){
         throw new AssertionError();
     }
 }
--- src/main/java/dev/ghen/thirst/foundation/mixin/supplementaries/MixinWaterBlockInteraction.java
+++ src/main/java/dev/ghen/thirst/foundation/mixin/supplementaries/MixinWaterBlockInteraction.java
@@ -3,23 +3,17 @@ package dev.ghen.thirst.foundation.mixin.supplementaries;
 import dev.ghen.thirst.content.purity.WaterPurity;
 import dev.ghen.thirst.foundation.mixin.accessors.supplementaries.FaucetBehaviorsManagerAccessor;
 import net.mehvahdjukaar.moonlight.api.fluids.BuiltInSoftFluids;
-import net.mehvahdjukaar.moonlight.api.fluids.SoftFluid;
 import net.mehvahdjukaar.moonlight.api.fluids.SoftFluidTank;
-import net.mehvahdjukaar.supplementaries.common.block.faucet.FaucetBehaviorsManager;
 import net.mehvahdjukaar.supplementaries.common.block.tiles.FaucetBlockTile;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(targets = "net.mehvahdjukaar.supplementaries.common.block.faucet.WaterBlockInteraction")
@@ -33,7 +27,7 @@ public class MixinWaterBlockInteraction
         CompoundTag purityTag = new CompoundTag();
         purityTag.putInt("Purity", WaterPurity.getBlockPurity(level, pos));
 
-        FaucetBehaviorsManagerAccessor.invokePrepareToTransferBottle(faucetTank, (SoftFluid) BuiltInSoftFluids.WATER.get(), purityTag);
+        FaucetBehaviorsManagerAccessor.invokePrepareToTransferBottle(faucetTank, BuiltInSoftFluids.WATER.getHolder(), purityTag);
         if (fillAction == null || fillAction.tryExecute()) {
             cir.setReturnValue(InteractionResult.SUCCESS);
         }
