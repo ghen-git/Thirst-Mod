3620c2fe6b98dbc53be8f99acbd00e2d5b581061
eyeofflame <138283224+eyeofflame@users.noreply.github.com>
2025-01-22 15:59:51 +0800

    Create MixinBotania.java
    修复与botania的兼容

---
+++ src/main/java/dev/ghen/thirst/foundation/mixin/botania/MixinBotania.java
@@ -0,0 +1,35 @@
+package dev.ghen.thirst.foundation.mixin
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import vazkii.botania.forge.xplat.ForgeXplatImpl;
+import vazkii.botania.xplat.XplatAbstractions;
+
+
+@Mixin(ForgeXplatImpl.class)
+public abstract class TestMixin implements XplatAbstractions {
+    @Inject(method = "extractFluidFromPlayerItem",at = @At("HEAD"), cancellable = true,remap = false)
+    public void extractFluidFromPlayerItem(Player player, InteractionHand hand, Fluid fluid, CallbackInfoReturnable<Boolean> cir) {
+        cir.cancel();
+        ItemStack abc = player.getItemInHand(hand);
+        cir.setReturnValue(abc.getCapability(ForgeCapabilities.FLUID_HANDLER_ITEM).map((h) -> {
+            FluidStack ex = h.drain(new FluidStack(fluid,1000),IFluidHandler.FluidAction.SIMULATE);
+            boolean su = ex.getFluid() == fluid && ex.getAmount() == 1000;
+            if(su && !player.getAbilities().instabuild){
+                h.drain(new FluidStack(fluid,1000),IFluidHandler.FluidAction.EXECUTE);
+                player.setItemInHand(hand,h.getContainer());
+            }
+            return su;
+        }).orElse(false));
+    }
+}
