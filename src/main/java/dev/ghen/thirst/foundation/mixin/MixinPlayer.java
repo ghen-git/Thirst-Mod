d22688bbd20cb2a8dad653025652751418aecc5e
mlus <1319237806@qq.com>
2025-03-21 22:25:01 +0800

    noticed the former change forget about effects

--- src/main/java/dev/ghen/thirst/foundation/mixin/MixinPlayer.java
+++ src/main/java/dev/ghen/thirst/foundation/mixin/MixinPlayer.java
@@ -1,5 +1,6 @@
 package dev.ghen.thirst.foundation.mixin;
 
+import dev.ghen.thirst.content.purity.WaterPurity;
 import dev.ghen.thirst.content.thirst.PlayerThirst;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -16,6 +17,7 @@ public abstract class MixinPlayer
     public void onEatDrink(Level level, ItemStack item, CallbackInfoReturnable<ItemStack> cir)
     {
         Player player = (Player) ((Object) this);
+        WaterPurity.givePurityEffects(player, item);
         PlayerThirst.drink(item, player);
     }
 
