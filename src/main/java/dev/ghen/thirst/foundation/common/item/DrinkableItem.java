b707ce9636c1768c0941b52fcb4a88e797e66824
ArkVeil <40077783+ArkVeil@users.noreply.github.com>
2025-03-22 15:31:54 -0400

    Revert "fix effect not work"
    This reverts commit 4d2829079349afe65303c7d2ccc49426d973f589.

--- src/main/java/dev/ghen/thirst/foundation/common/item/DrinkableItem.java
+++ src/main/java/dev/ghen/thirst/foundation/common/item/DrinkableItem.java
@@ -1,5 +1,6 @@
 package dev.ghen.thirst.foundation.common.item;
 
+import dev.ghen.thirst.content.thirst.PlayerThirst;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.stats.Stats;
@@ -49,6 +50,10 @@ public class DrinkableItem extends Item
         if (player instanceof ServerPlayer)
         {
             CriteriaTriggers.CONSUME_ITEM.trigger((ServerPlayer)player, item);
+        }
+        if(player != null)
+        {
+            PlayerThirst.drink(item, player);
         }
 
         if (player != null)
--- src/main/java/dev/ghen/thirst/foundation/mixin/MixinPotionItem.java
+++ src/main/java/dev/ghen/thirst/foundation/mixin/MixinPotionItem.java
@@ -1,11 +1,18 @@
 package dev.ghen.thirst.foundation.mixin;
 
+import dev.ghen.thirst.content.thirst.PlayerThirst;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PotionItem;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(PotionItem.class)
 public class MixinPotionItem {
@@ -16,5 +23,14 @@ public class MixinPotionItem {
             instance.player.drop(stack, false);
         }
         return true;
+    }
+
+    @Inject(method = "finishUsingItem", at = @At("RETURN"), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void onFinishUsingItem(ItemStack item, Level level, LivingEntity livingEntity, CallbackInfoReturnable<ItemStack> cir, Player player)
+    {
+        if(player != null)
+        {
+            PlayerThirst.drink(item, player);
+        }
     }
 }
